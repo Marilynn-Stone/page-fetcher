e746d865668059367003a29d99fdb14bb7295878
Marilynn Stone <hello@marilynnstone.com>
2022-06-21 00:55:05 +0000

    finish base case, remove README

--- fetcher.js
+++ fetcher.js
@@ -2,18 +2,14 @@ const fs = require('fs');
 const request = require('request');
 const argString = process.argv.slice(2);
 
-
-const content = 'Some content!';
-
-request('http://www.google.com', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
-
-fs.writeFile('text', content, err => {
-  if (err) {
-    console.error(err);
-  }
-  // file written successfully
+request(`${argString[0]}`, (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  fs.writeFile(`${argString[1]}`, body, error => {
+    const fileSize = body.length;
+    if (error) {
+      console.error(error);
+    }
+    console.log(`Downloaded and saved ${fileSize} bytes to ${argString[1]}`);
+  });
 });
